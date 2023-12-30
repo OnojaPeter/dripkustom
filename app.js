c97f34f9e70b1723cb0c6ca06404cd3beb92b1b9
Benjamin Onoja <onojapeter90@gmail.com>
2023-12-30 15:17:03 +0000

    done the recently added sortBy

--- app.js
+++ app.js
@@ -76,13 +76,16 @@ const shoeSchema = new mongoose.Schema({
     name: String,
     price: Number,
     category: String,
+    createdAt: { type: Date, default: Date.now }
     // Add more fields as needed
 });
 const Shoe = mongoose.model('Shoe', shoeSchema);
 
 // const dummyItems = [
-//     { image: "/public/images/7d77740b692425fa411ddf2b45825f51.jpg",name: 'Item 1', price: 80, category: "nike" },
-//     { image: "/public/images/20201110_112950.jpg", name: 'Item 2', price: 50, category: "adidas" },
+//     { image: "/public/images/7d77740b692425fa411ddf2b45825f51.jpg",name: 'Nike 1', price: 80, category: "nike"},
+//     { image: "/public/images/20201110_112950.jpg", name: 'Nike 2', price: 50, category: "nike"},
+// { image: "/public/images/4ff9167ec73af7436ba02f8135192578.jpg", name: 'Adidas 1', price: 20, category: "adidas"},
+// { image: "/public/images/9d1e24162babca5f650de973eaec89c7.jpg", name: 'Adidas 2', price: 25, category: "adidas"},
 //     // Add more items as needed
 // ];
 // const populateDummyItems = async () => {
@@ -121,6 +124,8 @@ app.get('/', async (req, res) => {
             sortCriteria = { price: 1 }; // Sort by price ascending
         } else if (sort === 'desc') {
             sortCriteria = { price: -1 }; // Sort by price descending
+        } else if (sort === 'recent') {
+            shoes = await Shoe.find().sort({ createdAt: -1 });
         }
 
         if (category) {
