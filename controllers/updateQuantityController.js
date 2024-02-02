2b227c4bf0c50c7a46ce2dd35d64b9e65cc6ab63
Benjamin Onoja <onojapeter90@gmail.com>
2024-02-02 15:04:40 +0000

    fixed the update quantity after chaging the way cart items are stored with shoeId-number

--- controllers/updateQuantityController.js
+++ controllers/updateQuantityController.js
@@ -2,13 +2,12 @@ const cookieParser = require('cookie-parser');
 
 async function updateQuantityController (req, res) {
     const { shoeId, quantity } = req.body;
-    const shoeIdObject = shoeId.toString(); // Convert ObjectId to string
 
     try {
         const cartCookie = req.cookies.cart || '{}';
         const cart = JSON.parse(cartCookie);
         console.log(cart);
-        const cartItem = cart[shoeIdObject];
+        const cartItem = cart[shoeId];
 
         if (cartItem) {
             // Update the quantity in the cart object
@@ -19,7 +18,17 @@ async function updateQuantityController (req, res) {
 
             res.status(200).json({ message: 'Quantity updated in cart', updatedCartItems: cart });
         } else {
-            res.status(404).json({ error: 'Item not found in cart' });
+            // If the cart has a key with the shoeId-size format, update its quantity
+            const sizeKeys = Object.keys(cart).filter(key => key.startsWith(`${shoeId}-`));
+            if (sizeKeys.length > 0) {
+                sizeKeys.forEach(sizeKey => {
+                    cart[sizeKey].quantity = quantity;
+                });
+                res.cookie('cart', JSON.stringify(cart));
+                res.status(200).json({ message: 'Quantity updated in cart', updatedCartItems: cart });
+            } else {
+                res.status(404).json({ error: 'Item not found in cart' });
+            }
         }
     } catch (error) {
         console.error('Error updating quantity:', error);
